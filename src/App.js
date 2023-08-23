449fbd0fd122aeae15dee275447e04adcf56ab4c
alex-nazemi <alexandernazemi@gmail.com>
2023-08-23 23:36:21 +0100

    created all the components for the app, passing state up and down compomnents tree, conditional rendering of invalid input component based on user input

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-// TODO:
-// Build components 
-// manage state such as list of users
-// output list of users or error 
-
+import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
+
   return (
     <div>
-
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
     </div>
   );
 }
